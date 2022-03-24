1150e9ecfc6adfce4274aaee20c197a71c0654be
Azizo1212 <azizdali530@gmail.com>
2022-03-24 14:01:58 -0700

    b_t-12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,24 +7,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t size = 0, a = 0;
+	size_t size;
 
 	if (!tree)
 		return (0);
-	if (tree->left )
-	{
-		size ++;
-		a = 1;
-	}
-	if (tree->right)
-	{
-		size ++;
-		a = 1;
-	}
-	if (a == 0)
-	{
+	if (tree->left || tree->right)
+		size = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	else
 		size = 1;
-	}
 
 	return (size);
 }
