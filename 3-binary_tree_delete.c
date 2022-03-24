fbf34322e978410087665b46d3c4c521691b797f
Azizo1212 <azizdali530@gmail.com>
2022-03-24 12:20:56 -0700

    b_t-03

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,5 +9,9 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
 	free(tree);
 }
